735fe79102d5ae0d21b4025d94fabeeb5e8ad5ae
Ritik <160255700+gritik418@users.noreply.github.com>
2026-02-18 21:36:34 +0530

    feat: add fastapi server

--- app/main.py
+++ app/main.py
@@ -1,4 +1,17 @@
 from app.rag.service import ask_question
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
 
 history = []
 
