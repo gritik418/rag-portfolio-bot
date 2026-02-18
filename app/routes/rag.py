0bf36f303e11de491bf442a0c1c245d2f278ca29
Ritik <160255700+gritik418@users.noreply.github.com>
2026-02-18 22:05:11 +0530

    feat: add rag model

--- app/routes/rag.py
+++ app/routes/rag.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from app.services.rag_service import RagService
+from app.models.rag_schema import RAGQuery, RAGResponse
 
 router = APIRouter()
 rag_service = RagService()
 
-@router.post("/query")
-def query_rag(payload):
-    print(payload)
-    answer = rag_service.generate_response(query=payload)
+@router.post("/query", response_model=RAGResponse)
+def query_rag(payload: RAGQuery):
+    answer = rag_service.generate_response(query=payload.query)
 
     return {"answer": answer}
